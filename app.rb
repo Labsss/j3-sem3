6ab70b88e49abb6eaac2a7dc40e7be755eae5e31
Labsss <alexandre.labonne@gmail.com>
2019-04-17 15:30:48 +0200

    1st step

---
+++ app.rb
@@ -0,0 +1,13 @@
+require "bundler"
+Bundler.require
+
+require "scrapper"
+$:.unshift File.expand_path("/thp/semaine3/j3-sem3/lib/app/scrapper", "scrapper")
+
+MyClass.new.perform
+
+Scrapper.new.perform
+
+File.open("/thp/semaine3/j3-sem3/lib/app/scrapper","scrapper") do | tableau |
+    tableau.write(h_cities_email.to_json)
+end
